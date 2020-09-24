3c2dd366b87c1beac820aa78e7b7eb4169fa97c8
fnnntc <fntc@list.ru>
2020-09-24 22:01:06 +0200

    Delete conftest.py

--- relayr test/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture(scope="function")
-def browser(request):
-    #global driver
-    browser= webdriver.Chrome(executable_path="D:/Programming/relayr test/chromedriver.exe")
-    yield browser
-    #driver.maximize_window()
